a6408a5c0fb335650598f4b4e8890718445d461b
AxelRuiz-inbest <axel.ruiz@inbest.cloud>
2025-09-01 16:42:29 -0600

    cambio

--- proan/Program.cs
+++ proan/Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "DevOps Mejor Opcion");
+app.MapGet("/", () => "Despliegue comppleto");
 
 app.Run();
