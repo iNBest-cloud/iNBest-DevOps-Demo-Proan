0147190e16a6a52d8e678ec4fca5147417a794cd
AxelRuiz-inbest <axel.ruiz@inbest.cloud>
2025-09-01 12:59:05 -0600

    create deploy proan

--- proan/Program.cs
+++ proan/Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", () => "Hello World! esto es una Prueba");
 
 app.Run();
