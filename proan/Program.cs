ca6afdd5b2c98798ef6c51d7d643f47d7e110d8c
AxelRuiz-inbest <axel.ruiz@inbest.cloud>
2025-09-02 16:34:53 -0600

    Hola Mundo

--- proan/Program.cs
+++ proan/Program.cs
@@ -1,28 +1,6 @@
-using System;
-using System.Data.SqlClient;
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
 
-class Program
-{
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hola Mundo");
+app.MapGet("/", () => "Despliegue comppleto");
 
-        // 🚨 Código vulnerable a SQL Injection 🚨
-        Console.WriteLine("Ingrese su nombre de usuario:");
-        string userInput = Console.ReadLine();
-
-        string connectionString = "Server=localhost;Database=TestDB;User Id=sa;Password=Your_password123;";
-        using (SqlConnection conn = new SqlConnection(connectionString))
-        {
-            conn.Open();
-            string query = "SELECT * FROM Users WHERE username = '" + userInput + "'";
-            SqlCommand cmd = new SqlCommand(query, conn);
-            SqlDataReader reader = cmd.ExecuteReader();
-
-            while (reader.Read())
-            {
-                Console.WriteLine("Bienvenido " + reader["username"]);
-            }
-        }
-    }
-}
+app.Run();
