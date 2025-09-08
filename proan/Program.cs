b2924762db435db3f1ec998c4623064aa9fd390c
AxelRuiz-Inbest <axel.ruiz@inbest.cloud>
2025-09-08 16:54:45 -0600

    Despliegue QA

--- proan/Program.cs
+++ proan/Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Despliegue completo DEV");
+app.MapGet("/", () => "Despliegue completo QA");
 
 app.Run();
