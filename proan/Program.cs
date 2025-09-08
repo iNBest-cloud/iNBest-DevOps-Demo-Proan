902b59ef4ac5836da949f8a578b50a3f6f6162c7
AxelRuiz-inbest <axel.ruiz@inbest.cloud>
2025-09-08 17:44:39 -0600

    a

--- proan/Program.cs
+++ proan/Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Despliegue comppleto Ramas Main");
+app.MapGet("/", () => "Despliegue comppleto Ramas Main Escaneo cs");
 
 app.Run();
